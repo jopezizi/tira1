058849eef722546b0ef5c2562cbd88b55561f580
Joona Kääriäinen <kaariainenjoona@gmail.com>
2026-01-17 14:05:18 +0200

    test commit

--- Viikko 1/special.py
+++ Viikko 1/special.py
@@ -1,3 +1,7 @@
+""" Vuosi 2026 on erityinen vuosi, koska 2^2+0^2+2^2+6^2=44 eli vuosiluvun numeroiden neliöiden summassa jokainen numero on sama.
+Toteuta tiedostoon special.py funktio check_year, joka ilmoittaa, onko parametrina annettu vuosi erityinen vuosi. Funktion tulee palauttaa True tai False.
+Funktiosi toimintaa testataan suurella määrällä erilaisia testejä. Jokaisessa testissä vuosiluku on välillä 1000–9999. """
+
 def check_year(year):
     year = list(str(year))
     sum = 0
