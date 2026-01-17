9410f906d36ed3beac3516e1cba9f31748b91a1e
Joona Kääriäinen <kaariainenjoona@gmail.com>
2026-01-17 16:53:58 +0200

    added wordgrid.py, incomplete

---
+++ Viikko 1/wordgrid.py
@@ -0,0 +1,22 @@
+class WordFinder:
+    def set_grid(self, grid):
+        # TODO
+
+    def count(self, word):
+        # TODO
+
+if __name__ == "__main__":
+    grid = ["TIRATIRA",
+            "IRATIRAT",
+            "RATIRATI",
+            "ATIRATIR"]
+
+    finder = WordFinder()
+    finder.set_grid(grid)
+
+    print(finder.count("TIRA")) # 7 
+    print(finder.count("TA")) # 13
+    print(finder.count("RITARI")) # 3
+    print(finder.count("A")) # 8
+    print(finder.count("AA")) # 6
+    print(finder.count("RAITA")) # 0  
