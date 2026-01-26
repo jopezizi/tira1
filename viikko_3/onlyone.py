f87c7ac17a3ee4a50f82e3e9f254ad945b6aae29
Joona Kääriäinen <kaariainenjoona@gmail.com>
2026-01-26 20:22:41 +0200

    finished onlyone.py

---
+++ viikko_3/onlyone.py
@@ -0,0 +1,21 @@
+def find_number(numbers):
+    minnum = min(numbers)
+    maxnum = max(numbers)
+
+    if numbers.count(minnum) == 1:
+        return minnum
+    else:
+        return maxnum
+
+
+
+if __name__ == "__main__":
+    print(find_number([1, 1, 1, 2])) # 2
+    print(find_number([1, 1, 2, 1])) # 2
+    print(find_number([1, 2, 1, 1])) # 2
+    print(find_number([2, 1, 1, 1])) # 2
+    print(find_number([5, 5, 5, 3, 5])) # 3
+    print(find_number([1, 100, 1])) # 100
+
+    numbers = [1] * 10**5 + [2]
+    print(find_number(numbers)) # 2
